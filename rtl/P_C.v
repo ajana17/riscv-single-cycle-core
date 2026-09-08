a54228efccad7eb9dde46519c1b9e9bb40429435
ajana17 <153418224+ajana17@users.noreply.github.com>
2026-09-08 15:32:30 +0530

    Update P_C.v

--- rtl/P_C.v
+++ rtl/P_C.v
@@ -1,1 +1,17 @@
+`timescale 1ns / 1ps
 
+module P_C(
+    input clk,
+    input rst,
+    input  [31:0] PC_NEXT,
+    output reg [31:0] PC
+);
+
+    always @(posedge clk or negedge rst) begin
+        if (!rst)
+            PC <= 32'd0;
+        else
+            PC <= PC_NEXT;
+    end
+
+endmodule
