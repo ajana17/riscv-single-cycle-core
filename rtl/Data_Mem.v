5cd33d59c4e595cb33484f96a68b979edae37a36
ajana17 <153418224+ajana17@users.noreply.github.com>
2026-09-08 15:29:37 +0530

    Update Data_Mem.v

--- rtl/Data_Mem.v
+++ rtl/Data_Mem.v
@@ -1,1 +1,19 @@
+`timescale 1ns / 1ps
+module Data_Mem(
+    input clk, rst, WE,
+    input  [31:0] A, WD,
+    output [31:0] RD
+);
+    reg [31:0] Data_MEM [0:1023];
 
+    assign RD = (!rst || WE) ? 32'd0 : Data_MEM[A[31:2]];
+
+    always @(posedge clk) begin
+        if (WE)
+            Data_MEM[A[31:2]] <= WD;
+    end
+
+    initial begin
+        Data_MEM[7] = 32'h00000020;
+    end
+endmodule
