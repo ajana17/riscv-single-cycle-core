d76625083f5e0095d86461d7dd2ca63e041de56b
ajana17 <153418224+ajana17@users.noreply.github.com>
2026-09-08 15:37:41 +0530

    Update tb_risc.v

--- tb/tb_risc.v
+++ tb/tb_risc.v
@@ -1,1 +1,24 @@
+`timescale 1ns / 1ps
 
+module tb_riscv;
+    reg clk;
+    reg rst;
+
+    RISC_V_Single_Cycle_Top dut (
+        .clk(clk),
+        .rst(rst)
+    );
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        rst = 0;
+
+        #3;
+        rst = 1;
+
+        #250;
+        $finish;
+    end
+endmodule
