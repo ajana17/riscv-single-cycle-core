230d0fe9c6bd1dcf91819189e593b33f82d21e4e
ajana17 <153418224+ajana17@users.noreply.github.com>
2026-09-08 15:28:55 +0530

    Update Control_Unit_Top.v

--- rtl/Control_Unit_Top.v
+++ rtl/Control_Unit_Top.v
@@ -1,1 +1,34 @@
+`timescale 1ns / 1ps
+module Control_Unit_Top(
+    input  [6:0] Op,
+    input  [2:0] funct3,
+    input        funct7_5,
+    output       RegWrite,
+    output [1:0] ImmSrc,
+    output       ALUSrc,
+    output       MemWrite,
+    output       ResultSrc,
+    output       Branch,
+    output [2:0] ALUControl
+);
+    wire [1:0] ALUOp;
 
+    main_decoder mainpart (
+        .op(Op),
+        .RegWrite(RegWrite),
+        .MemWrite(MemWrite),
+        .ResultSrc(ResultSrc),
+        .ALUSrc(ALUSrc),
+        .Branch(Branch),
+        .ImmSrc(ImmSrc),
+        .ALUOp(ALUOp)
+    );
+
+    alu_decoder alupart (
+        .ALUOp(ALUOp),
+        .op5(Op[5]),
+        .funct3(funct3),
+        .funct7_5(funct7_5),
+        .control(ALUControl)
+    );
+endmodule
