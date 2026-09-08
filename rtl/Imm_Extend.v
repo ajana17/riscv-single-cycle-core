d79e4afde9b3733a2872a4e34bbe22c49e367b1a
ajana17 <153418224+ajana17@users.noreply.github.com>
2026-09-08 15:30:03 +0530

    Update Imm_Extend.v

--- rtl/Imm_Extend.v
+++ rtl/Imm_Extend.v
@@ -1,1 +1,15 @@
-
+`timescale 1ns / 1ps
+module Imm_Extend(
+    input  [31:7] instr,
+    input  [1:0]  immsrc,
+    output reg [31:0] immext
+);
+    always @(*) begin
+        case (immsrc)
+            2'b00: immext = {{20{instr[31]}}, instr[31:20]};                                // I-type
+            2'b01: immext = {{20{instr[31]}}, instr[31:25], instr[11:7]};                  // S-type
+            2'b10: immext = {{20{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0}; // B-type
+            default: immext = 32'd0;
+        endcase
+    end
+endmodule
